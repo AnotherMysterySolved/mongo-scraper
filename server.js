6f5a3f94bbcedd7f85cbae59fda5b28f52d8cfcd
AnotherMysterySolved <14mprice@gmail.com>
2017-06-06 13:28:32 -0400

    deploy to heroku

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 // Database configuration with mongoose
-mongoose.connect("mongodb://localhost/week18day3mongoose");
+mongoose.connect("mongodb://heroku_153dc2m7:4et23o11uc5cqjejnb7o9r2ug6@ds123361.mlab.com:23361/heroku_153dc2m7");
 var db = mongoose.connection;
 
 // Show any mongoose errors
